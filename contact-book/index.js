95740c42684a30cfe386673767a26db78b9e55f4
kimkyoungcheon <rudcjs42@naver.com>
2021-10-11 18:24:22 +0900

    주소록-index,new,create

--- contact-book/index.js
+++ contact-book/index.js
@@ -8,20 +8,54 @@ const app = express();
 mongoose.connect(process.env.MONGO_DB); // 1
 const db = mongoose.connection; //2
 //3
-db.once("open", function () {
+db.once("open", () => {
   console.log("DB connected");
 });
 //4
-db.on("error", function (err) {
+db.on("error", (err) => {
   console.log("DB ERROR : ", err);
 });
 
 // Other settings
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// DB schema // 4
+const contactSchema = mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  email: { type: String },
+  phone: { type: String },
+});
+const Contact = mongoose.model("contact", contactSchema); // 5
+
+// Routes
+// Home // 6
+app.get("/", (req, res) => {
+  res.redirect("/contacts");
+});
+// Contacts - Index // 7
+app.get("/contacts", (req, res) => {
+  Contact.find({}, (err, contacts) => {
+    if (err) return res.json(err);
+    res.render("contacts/index", { contacts: contacts });
+  });
+});
+// Contacts - New // 8
+app.get("/contacts/new", (req, res) => {
+  res.render("contacts/new");
+});
+// Contacts - create // 9
+app.post("/contacts", (req, res) => {
+  Contact.create(req.body, (err, contact) => {
+    if (err) return res.json(err);
+    res.redirect("/contacts");
+  });
+});
 
 // Port setting
-const port = 3000;
-app.listen(port, function () {
+const port = process.env.PORT;
+app.listen(port, () => {
   console.log("server on! http://localhost:" + port);
 });
